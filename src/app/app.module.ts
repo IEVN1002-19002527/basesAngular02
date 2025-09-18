62967a55271c1326edf384ebc845e11df6a9aa19
Oscar Meza <meza03994@gmail.com>
2025-09-17 20:56:58 -0600

    pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeroesListComponent } from './heroes/heroes-list/heroes-list.component';
+import { FormsModule } from '@angular/forms';
+import { HeroesFilterPipe } from './heroes/heroes-filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesListComponent
+    HeroesListComponent,
+    HeroesFilterPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes-list/heroes-list.component.ts
+++ src/app/heroes/heroes-list/heroes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Heroes } from '../heroes';
 
 @Component({
   selector: 'app-heroes-list',
@@ -7,30 +8,37 @@ import { Component } from '@angular/core';
 })
 export class HeroesListComponent {
 
-  heroes:any[]=[
+  listFilter:string='';
+
+  muestraImg:boolean=true;
+  showImage(){
+    this.muestraImg=!this.muestraImg;
+  }
+
+  heroes:Heroes[]=[
     {
-      "Imagen":"https://dragonball-api.com/transformaciones/goku_ssj.webp",
+      Imagen:"https://dragonball-api.com/transformaciones/goku_ssj.webp",
       Nombre:"Goku",
       Descripcion:"Kakaroto",
       Race:"Saiyan",
       Ki:9000
     },
     {
-      "Imagen":"https://dragonball-api.com/transformaciones/vegeta SSJ (2).webp",
+      Imagen:"https://dragonball-api.com/transformaciones/vegeta SSJ (2).webp",
       Nombre:"Vegeta",
       Descripcion:"Z Fighter",
       Race:"Saiyan",
       Ki:2500
     },
     {
-      "Imagen":"https://dragonball-api.com/transformaciones/Piccolo super namekiano.webp",
+      Imagen:"https://dragonball-api.com/transformaciones/Piccolo super namekiano.webp",
       Nombre:"Piccolo",
       Descripcion:"Z Fighter",
       Race:"Namekian",
       Ki:2000
     },
     {
-      "Imagen":"https://dragonball-api.com/characters/bulma.webp",
+      Imagen:"https://dragonball-api.com/characters/bulma.webp",
       Nombre:"Bulma",
       Descripcion:"Z Fighter",
       Race:"Humano",
